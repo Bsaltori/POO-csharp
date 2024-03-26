8657b68b8d37e8db4b2fd7019c38d2c2517e1476
Bsaltori <biasaltori@yahoo.com.br>
2024-03-26 09:47:03 -0300

    Exercicio area do retangulo

---
+++ Ex03/Ex03/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Formats.Asn1;
+using System.Globalization;
+
+namespace Ex03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Retangulo retangulo = new Retangulo();
+            Console.WriteLine("Entre a largura e altura do retângulo: ");
+            retangulo.Largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            retangulo.Altura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double area =  retangulo.Area(retangulo.Largura, retangulo.Altura);
+            Console.WriteLine("AREA = " + area.ToString("F2", CultureInfo.InvariantCulture));
+
+            double perimetro = retangulo.Perimetro(retangulo.Largura, retangulo.Altura);
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F2", CultureInfo.InvariantCulture));
+
+            double diagonal = retangulo.Diagonal(retangulo.Largura, retangulo.Altura);
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F2", CultureInfo.InvariantCulture));
+
+
+
+
+        }
+    }
+}
---
+++ Ex03/Ex03/Retangulo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ex03
+{
+    internal class Retangulo
+    {
+        public double Largura;
+        public double Altura;
+
+        public double Area(double Largura, double Altura)
+        {
+            return Largura * Altura;
+        }
+
+        public double Perimetro(double Largura, double Altura)
+        {
+            
+            return 2.0 * (Largura + Altura);
+        }
+
+        public double Diagonal(double Largura, double Altura)
+        {
+            return Math.Sqrt(Math.Pow(Altura, 2) + Math.Pow(Largura, 2));
+        }
+
+    }
+
+
+}
